e3f5e99615dcf1dc62378b1214ca083e11d0ccae
raphaelcanuto464@gmail.com <raphaelcanuto464@gmail.com>
2024-03-07 12:03:12 -0300

    Chore

---
+++ Aula 4 - 29.02.24/Iniciando/Iniciando/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Iniciando
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Aula 4 - 29.02.24/Iniciando/Iniciando/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Iniciando
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
