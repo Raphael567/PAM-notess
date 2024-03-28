e499e2dfe9fed1bce61e07550c968219836f80ee
Raphael567 <raphaelcanuto464@gmail.com>
2024-03-28 12:11:08 -0300

    Atualizando Arquivos

--- notes/notes/NotesPage.xaml.cs
+++ notes/notes/NotesPage.xaml.cs
@@ -3,12 +3,17 @@ namespace notes;
 public partial class NotesPage : ContentPage
 {
     //DCIM/MEDIA/com.whatsapp.media
-    string path = Path.Combine(FileSystem.AppDataDirectory,"nota.txt");
-    string content = string.Empty;
+    string path = Path.Combine(FileSystem.AppDataDirectory,"notes.txt");
+    string content = "";
 
 	public NotesPage()
 	{
 		InitializeComponent();
+        if (File.Exists(path))
+        {
+            //Carrega o conteúdo do arquivo no editor de texto
+            FileEditor.Text = File.ReadAllText(path);
+        }
 	}
 
     private void SaveButton_Clicked(object sender, EventArgs e)
@@ -18,10 +23,17 @@ public partial class NotesPage : ContentPage
 
         //Criar o arquivo com esse conteúdo
         File.WriteAllText(path, content);
+        DisplayAlert("Sucesso", "Arquivo salvo com sucesso", "OK");
     }
 
     private void DeleteButton_Clicked(object sender, EventArgs e)
     {
-
+        if (File.Exists(path))
+        {
+            File.Delete(path);
+            FileEditor.Text = "";
+            //Alert
+            DisplayAlert("Sucesso", "Arquivo deletado com sucesso", "OK");
+        }
     }
 }
